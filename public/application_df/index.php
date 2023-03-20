7ee934882c5ca7691c76be2a1b0dd3c2524e3ee2
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2023-03-19 22:08:43 -0300

    Adding alertmanager

--- public/application_df/index.php
+++ public/application_df/index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo 'Application MG';
+try {
+	$pdo = new PDO(
+        'mysql:host=database_df;dbname=application_df;charset=UTF8', 
+        'root', 
+        'root'
+    );
+
+	if ($pdo) {
+		echo "Connected to the database_df database successfully!";
+	}
+} catch (PDOException $e) {
+	echo $e->getMessage();
+}
--- public/application_rs/index.php
+++ public/application_rs/index.php
@@ -1,3 +1,15 @@
 <?php
 
-phpinfo();
+try {
+	$pdo = new PDO(
+        'mysql:host=database_rs;dbname=application_rs;charset=UTF8', 
+        'root', 
+        'root'
+    );
+
+	if ($pdo) {
+		echo "Connected to the database_rs database successfully!";
+	}
+} catch (PDOException $e) {
+	echo $e->getMessage();
+}
